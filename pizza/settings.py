bcc5ef83549fd808b36d6bb78cb2adfdaf1781a3
Hemabh Kamboj <hkamboj2603199@gmail.com>
2019-03-13 02:01:18 +0530

    configured for heroku

--- pizza/settings.py
+++ pizza/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
